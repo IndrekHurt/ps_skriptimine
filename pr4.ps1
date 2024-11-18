2c8d73f128cc5bbcac1e910889d3731cf61437c3
Indrek Hurt <indrek.hurt.001@voco.ee>
2024-11-18 09:52:05 +0200

    ii part- asking the user to choose a country, then giving the result with the country's capital

---
+++ pr4.ps1
@@ -0,0 +1,18 @@
+﻿# ii part
+
+$countries = @('India', 'Australia', 'China')
+$capitals = @('New Delhi', 'Canberra', 'Beijing')
+
+Write-Host "Select country" -ForegroundColor Yellow
+
+for($i = 1; $i -le $countries.Count; $i++){
+    Write-Host "$i. "$countries[$i-1] -ForegroundColor Green
+}
+
+$country = Read-Host "Please select country"
+
+if($country -le $countries.Count){
+    Write-Host $countries[$country-1]"'s capital is " $capitals[$country-1] -ForegroundColor Green
+} else {
+    Write-Host "Entered value is wrong" -ForegroundColor Red
+}

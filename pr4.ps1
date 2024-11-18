e5a492806f57c7da62f60b061c66cf2e887da851
Indrek Hurt <indrek.hurt.001@voco.ee>
2024-11-18 10:02:55 +0200

    i part- asking for the user to insert 2 values and comparing them with displaying which value is bigger

--- pr4.ps1
+++ pr4.ps1
@@ -1,18 +1,13 @@
-﻿# ii part
+﻿# i part
 
-$countries = @('India', 'Australia', 'China')
-$capitals = @('New Delhi', 'Canberra', 'Beijing')
+$value1 = Read-Host "Enter the first value"
+$value2 = Read-Host "Enter the second value"
 
-Write-Host "Select country" -ForegroundColor Yellow
-
-for($i = 1; $i -le $countries.Count; $i++){
-    Write-Host "$i. "$countries[$i-1] -ForegroundColor Green
-}
-
-$country = Read-Host "Please select country"
-
-if($country -le $countries.Count){
-    Write-Host $countries[$country-1]"'s capital is " $capitals[$country-1] -ForegroundColor Green
-} else {
-    Write-Host "Entered value is wrong" -ForegroundColor Red
-}
+if($value1 -gt $value2)
+  {
+    Write-Host "The higher number is : "$value1
+  }
+else
+  {
+    Write-Host "The higher number is : "$value2
+  }

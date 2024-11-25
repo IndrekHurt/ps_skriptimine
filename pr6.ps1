4f29bad735d327e597c702d0b54b7a1a0173bc2e
Indrek Hurt <indrek.hurt.001@voco.ee>
2024-11-25 08:51:40 +0200

    get the result from the list (fixed)

--- pr6.ps1
+++ pr6.ps1
@@ -1,1 +1,19 @@
-﻿
+﻿$students = Inport-Csv C:\Users\Indrek\ps_skriptimine\ages.csv
+
+$schools = @()
+$names = @()
+$schoolNames = @()
+
+
+foreach($student in $students){
+    $school= ""
+    if([int32]$student.Age -ge 4 -and [int32]$student.Age -le 10){
+        $school = "Junior"        
+    } elseif ([int32]$student.Age -ge 11 -and [int32]$student.Age -le 17) {
+        $school = "Senior"
+    }
+    $schools += @{"Name"=$student.Name; "school"=$schoolNames}
+
+}
+
+$schools | Select-Object -Property Name, School | Export-Csv -NoTypeInformation C:\Users\Indrek\ps_skriptimine\schools.csv

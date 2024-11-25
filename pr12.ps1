2516b61b2df333800d4a4ca70c93dfe8487d6214
Indrek Hurt <indrek.hurt.001@voco.ee>
2024-11-25 11:25:50 +0200

    Sevice status

---
+++ pr12.ps1
@@ -0,0 +1,14 @@
+﻿function countstart {
+    $services = Get-Service | Where {$_.Status -like 'Running'}
+    $services.Count
+}
+
+function countstop {
+    $services = Get-Service | Where {$_.Status -like 'Stopped'}
+    $services.Count
+}
+
+function countstart {
+    $services = Get-Service | Where {$_.Status -like 'Running'}
+    $services.Count
+}

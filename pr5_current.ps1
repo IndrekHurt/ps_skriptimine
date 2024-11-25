73acffc9c2fd8b91bac1a54e776ad473a8c2baa9
Indrek Hurt <indrek.hurt.001@voco.ee>
2024-11-25 10:46:38 +0200

    File size

--- pr5_current.ps1
+++ pr5_current.ps1
@@ -1,1 +1,8 @@
-﻿(Get-Process).Where({$_.ProcessName -eq 'notepad'}) | Select ProcessName, Id
+﻿Get-ChildItem -Path "C:\Users\Indrek\ps_skriptimine"
+
+$result = Get-ChildItem -Path C:\Users\Indrek\ps_skriptimine\* -Include *csv | Select Name, Length
+
+Write-Host
+Write-Host "Filename": $result.Name
+Write-Host "Size in KB": $($result.Lenth/1KB)
+Write-Host "Size in MB": $($result.Length/1MB)
